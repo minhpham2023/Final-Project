4ca17ce743a795230ca390814f911cdcee2d4bb4
minhpham2023 <pmtuan2017@gmail.com>
2022-11-29 18:25:23 -0500

    third commit

--- app/src/main/java/com/example/myandroidapp/MainActivity.kt
+++ app/src/main/java/com/example/myandroidapp/MainActivity.kt
@@ -1,11 +1,26 @@
 package com.example.myandroidapp
 
-import androidx.appcompat.app.AppCompatActivity
+import android.content.Intent
 import android.os.Bundle
+import androidx.appcompat.app.AppCompatActivity
+import kotlinx.android.synthetic.main.activity_main.*
 
 class MainActivity : AppCompatActivity() {
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
         setContentView(R.layout.activity_main)
+
+        btnNext.setOnClickListener {
+            val intent = Intent(this, SecondActivity2::class.java)
+            intent.putExtra("Username", etUsername.text.toString())
+            startActivity(intent)
+        }
+
+
+
+
+
     }
+
+
 }
--- app/src/main/java/com/example/myandroidapp/SecondActivity2.kt
+++ app/src/main/java/com/example/myandroidapp/SecondActivity2.kt
@@ -1,11 +1,15 @@
 package com.example.myandroidapp
 
-import androidx.appcompat.app.AppCompatActivity
 import android.os.Bundle
+import androidx.appcompat.app.AppCompatActivity
+import kotlinx.android.synthetic.main.activity_second2.*
 
 class SecondActivity2 : AppCompatActivity() {
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
         setContentView(R.layout.activity_second2)
+
+        val wcuser = intent.getStringExtra("Username")
+        tvWelcome.text = "Welcome $wcuser"
     }
 }
